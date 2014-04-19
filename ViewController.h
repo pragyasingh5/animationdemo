c3178352d7d0659c79356455eddcafe5dcdc9956
pragya5 <pragya.singh.mk@gmail.com>
2014-04-18 17:54:58 -0700

    Create ViewController.h

---
+++ ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Animation2
+//
+//  Created by Pragya Singh on 4/10/14.
+//  Copyright (c) 2014 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+    
+    IBOutlet UIImageView *animation;
+}
+
+@end
