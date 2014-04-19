dcaffb528d0c38ba7847847f0ebf4e4ca261634d
pragya5 <pragya.singh.mk@gmail.com>
2014-04-18 17:54:08 -0700

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Animation2
+//
+//  Created by Pragya Singh on 4/10/14.
+//  Copyright (c) 2014 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
